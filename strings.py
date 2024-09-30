1def58d4ddc3801937476a466d73f26fb04eb920
Spanioulis <pani77sps@gmail.com>
2024-09-30 15:50:51 +0200

    Add strings file

--- strings.py
+++ strings.py
@@ -1,4 +1,24 @@
 # Strings
 
 myString = "Hello World"
-dir(myString)
+# print(dir(myString))
+
+# print(myString.upper())
+# print(myString.lower())
+# print(myString.swapcase())
+# print(myString.capitalize())
+
+# print(myString.replace("Hello", "Hola"))
+# print(myString.startswith("Hello"))
+# print(myString.startswith("hello"))
+# print(myString.count("l"))
+# print(myString.find("l"))
+
+# print(myString[6])
+
+# Concatenando
+my_name = "Sergio"
+my_age = 30
+
+print("Ni nombre es " + my_name + " y tengo una edad de " + str(my_age) + "años")
+print(f"Ni nombre es {my_name} y tengo una edad de {my_age} años")
