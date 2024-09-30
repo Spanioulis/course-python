8f70e92fba667714540805bae25ba16f0c503543
Spanioulis <pani77sps@gmail.com>
2024-09-30 16:26:50 +0200

    Add numbers and lists files

---
+++ lists.py
@@ -0,0 +1,38 @@
+# Lists
+
+# demo_list
+demo_list = [1, "helo", True, False, [1, 2, 3, 4]]
+print(demo_list)
+
+# range
+r = list(range(1, 25))
+# print(r)
+
+# for item in r: 
+#   print(item + 25)
+
+# colors
+colors = ['red', 'green', 'blue']
+
+# colors.append('yellow')
+# colors.insert(1, 'orange')
+# colors.remove('blue')
+# colors.pop(0)
+# colors.reverse()
+# colors.sort()
+
+print(len(colors))
+# print(dir(colors))
+colors.append('crimson')
+print(len(colors))
+print(sorted(colors))
+colors.extend(['red', 'orange'])
+colors.extend(['violet', 'white', 'black'])
+
+print(colors)
+colors.pop(0)
+print(colors)
+colors.remove('red')
+colors.remove('green')
+colors.remove('blue')
+print(colors)
---
+++ numbers.py
@@ -0,0 +1,6 @@
+# Numbers
+
+age = input("Enter your age: ")
+# Importante convertir a int, ya que la edad es un string
+age = 5 + int(age)
+print(f"Your age is {age}")
